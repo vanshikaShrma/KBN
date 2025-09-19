c0db6276c2e1d741e8d33a1a4cfcf7e0580c2d6a
Admin <Admin@DESKTOP-8EPD97D>
2025-09-19 11:27:27 +0530

    Add project files.

---
+++ KBN/Services/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using KBN.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+
+namespace KBN.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly EmailSettings _settings;
+
+        public EmailSender(IOptions<EmailSettings> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            var smtpClient = new SmtpClient(_settings.SmtpServer)
+            {
+                Port = _settings.Port,
+                Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(_settings.SenderEmail, _settings.SenderName),
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(email);
+
+            return smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
+
---
+++ KBN/Services/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace KBN.Services
+{
+    public class EmailSettings
+    {
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
